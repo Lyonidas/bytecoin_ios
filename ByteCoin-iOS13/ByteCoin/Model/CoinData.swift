2e7a105a9c70b8f1e1417be3bf1119dc054b4218
Preston Hudson <prestonleehudson@gmail.com>
2020-03-12 11:17:45 -0500

    Updated ByteCoin to support iOS13.

---
+++ ByteCoin-iOS13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Preston Hudson on 11/09/2019.
+//  Copyright © 2020 Spade Software. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
